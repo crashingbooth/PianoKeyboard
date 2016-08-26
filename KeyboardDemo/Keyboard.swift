d3c5f72d9a6f4c8ae9c8ce5dfa820de1f46583a9
crashing booth <jholtzkener@hotmail.com>
2016-08-26 09:36:02 +0900

    cleaned up code

--- KeyboardDemo/Keyboard.swift
+++ KeyboardDemo/Keyboard.swift
@@ -11,7 +11,7 @@ import UIKit
 @IBDesignable class Keyboard: UIView, PianoKeyDelegate{
     let keyPattern:[PianoKey.KeyType] = [.White, .Black, .White, .Black, .White, .White, .Black, .White,.Black, .White, .Black, .White]
     let blackKeyOffset:[CGFloat] = [4.0, 5.5, 0.0, 4.0, 5.0, 6.0, 0.0] // measured from Roland A-500 keyboard
-    let numWhiteKeys = 15
+    let numWhiteKeys = 19
     let root:UInt8 = 48
     var whiteKeyWidth: CGFloat {
         get { return self.bounds.width / CGFloat(numWhiteKeys)}
@@ -33,18 +33,17 @@ import UIKit
     }
 
     var view: UIView!
+    
+    
+    // MARK: - Init and SetUp
     override init(frame: CGRect) {
-        // 1. setup any properties here
         super.init(frame: frame)
         setUp()
-
     }
     
     required init?(coder aDecoder: NSCoder) {
-        // 1. setup any properties here
         super.init(coder: aDecoder)
         setUp()
-
     }
     
     func setUp() {
@@ -53,44 +52,62 @@ import UIKit
         addPianoKeysWithCurrentFrames()
     }
     
-    
-    private func getKeyOfTouch(touch: UITouch) -> PianoKey? {
-        let loc = touch.locationInView(self)
-        var selection: PianoKey?
-        var selectedKeys = [PianoKey]()
-        for key in pianoKeys {
-            if key.frame.contains(loc) {
-                selectedKeys.append(key)
-            }
-        }
-        
-        // only one key must be white
-        if selectedKeys.count == 1 {
-            selection = selectedKeys[0]
-        } else {
-            // if multiple keys, only press black key
-            for key in selectedKeys {
-                if key.keyType == PianoKey.KeyType.Black {
-                    selection = key
-                    break
-                }
-            }
-        }
-        return selection
-
+    override func layoutSubviews() {
+        addPianoKeysWithCurrentFrames()
     }
     
-    // MARK; - Override Touch Methods
+    private func createKeys() {
+        var whiteKeyNum = 0
+        var absoluteNum = 0
+        var currentType: PianoKey.KeyType!
+        while whiteKeyNum <= numWhiteKeys {
+            currentType = keyPattern[absoluteNum % 12]
+            if currentType == PianoKey.KeyType.White {
+                whiteKeyNum += 1
+            }
+            if whiteKeyNum == numWhiteKeys && currentType == .Black { break }
+            let key = PianoKey(frame: CGRect.zero, midiNoteNumber: root + UInt8(absoluteNum), type: currentType)
+            
+            
+            pianoKeys.append(key)
+            absoluteNum += 1
+        }
+    }
+    
+    private func addPianoKeysWithCurrentFrames() {
+        for key in pianoKeys {
+            key.removeFromSuperview()
+        }
+        
+        var whiteKeyNum = 0
+        for key in pianoKeys {
+            let keyFrame: CGRect!
+            if key.keyType == PianoKey.KeyType.White {
+                keyFrame = CGRect(x: CGFloat(whiteKeyNum) * whiteKeyWidth, y: 0, width: whiteKeyWidth, height: whiteKeyHeight)
+                whiteKeyNum += 1
+            } else {
+                let offset = blackKeyOffset[(whiteKeyNum - 1) % 7]
+                keyFrame = CGRect(x: CGFloat(whiteKeyNum - 1) * whiteKeyWidth + whiteKeyWidth * (offset/7.0), y: 0, width: blackKeyWidth, height: blackKeyHeight)
+            }
+            key.frame = keyFrame
+            addSubview(key)
+        }
+        
+        // put black keys on top
+        for key in pianoKeys {
+            if key.keyType == PianoKey.KeyType.Black {
+                bringSubviewToFront(key)
+            }
+        }
+    }
+    
+    
+    // MARK: - Override Touch Methods
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        print("numTouhces \(touches.count), in events \(event!.allTouches()!.count)")
         for touch in touches {
             if let key = getKeyOfTouch(touch) {
                 pressAdded(key)
-                let noteSet = getNoteSetFromTouches(touches)
-                print("noteSet in touchesBegan: \(noteSet)")
-                var allTouches = event?.allTouches() ?? Set<UITouch>()
-                allTouches.insert(touch)
-            verifyTouches(allTouches)
+                verifyTouches(event?.allTouches() ?? Set<UITouch>())
             }
         }
     }
@@ -98,8 +115,7 @@ import UIKit
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
         for touch in touches {
             if !self.frame.contains(touch.locationInView(self)) {
-               verifyTouches(event?.allTouches() ?? Set<UITouch>())
-//                findOldKeyFromTouchAndRelease(touch, newKey: nil)
+                verifyTouches(event?.allTouches() ?? Set<UITouch>())
             } else {
                 if let key = getKeyOfTouch(touch) {
                     pressAdded(key)
@@ -134,25 +150,38 @@ import UIKit
         
     }
     
+    // MARK: - Identify Keys
+    private func getKeyOfTouch(touch: UITouch) -> PianoKey? {
+        let loc = touch.locationInView(self)
+        var selection: PianoKey?
+        var selectedKeys = [PianoKey]()
+        for key in pianoKeys {
+            if key.frame.contains(loc) {
+                selectedKeys.append(key)
+            }
+        }
+        
+        // if only one key, must be white
+        if selectedKeys.count == 1 {
+            selection = selectedKeys[0]
+        } else {
+            // if multiple keys (b/c keys overlap white), only select black key
+            for key in selectedKeys {
+                if key.keyType == PianoKey.KeyType.Black {
+                    selection = key
+                    break
+                }
+            }
+        }
+        return selection
+    }
     
-//    private func checkKeysForTouch(touch:UITouch) {
-//        if let key = getKeyOfTouch(touch) {
-//            pressAdded(key)
-////            findOldKeyFromTouchAndRelease(touch, newKey: key)
-//        }
-//    }
-//    
-//    // maybe don't care about newkey, b/c checking alltouches
-//    private func findOldKeyFromTouchAndRelease(touch: UITouch, newKey: PianoKey? = nil) {
-//        let oldLoc = touch.previousLocationInView(self)
-//        for key in pianoKeys {
-//            if key.frame.contains(oldLoc) && key != newKey {
-//                pressRemoved(key)
-//            }
-//        }
-//    }
-  
-    
+    private func getKeyFromMidiNote(midiNoteNumber: UInt8) -> PianoKey {
+        let index = Int(midiNoteNumber - root)
+        return pianoKeys[index]
+    }
+
+    // MARK: - Handling Keys
     private func pressAdded(key: PianoKey) {
         if key.pressed() {
             pressedKeys.insert(key.midiNoteNumber)
@@ -161,19 +190,18 @@ import UIKit
     }
     
     private func pressRemoved(key: PianoKey) {
-        key.released()
-        pressedKeys.remove(key.midiNoteNumber)
-        print("released!!! \(key.midiNoteNumber), \(pressedKeys)")
+        if key.released() {
+            pressedKeys.remove(key.midiNoteNumber)
+            print("released \(key.midiNoteNumber), \(pressedKeys)")
+        }
     }
     
-    
+    // MARK: - Verify Keys
     private func getNoteSetFromTouches(touches: Set<UITouch>) -> Set<UInt8> {
         var touchedKeys = Set<UInt8>()
-        print("num touches in getNoteSet: \(touches.count)")
         for touch in touches {
             if let key = getKeyOfTouch(touch) {
                 touchedKeys.insert(key.midiNoteNumber)
-                print("found key: \(key.midiNoteNumber)")
             }
         }
         return touchedKeys
@@ -192,60 +220,8 @@ import UIKit
         }
     }
     
-    
-    
-    private func getKeyFromMidiNote(midiNoteNumber: UInt8) -> PianoKey {
-        let index = Int(midiNoteNumber - root)
-        return pianoKeys[index]
-    }
-    
    
-    // MARK: - SetUp and Geometry
-    private func createKeys() {
-        var whiteKeyNum = 0
-        var absoluteNum = 0
-        var currentType: PianoKey.KeyType!
-        while whiteKeyNum <= numWhiteKeys {
-            currentType = keyPattern[absoluteNum % 12]
-            if currentType == PianoKey.KeyType.White {
-                 whiteKeyNum += 1
-            }
-            if whiteKeyNum == numWhiteKeys && currentType == .Black { break }
-            let key = PianoKey(frame: CGRect.zero, midiNoteNumber: root + UInt8(absoluteNum), type: currentType)
 
-            
-            pianoKeys.append(key)
-            absoluteNum += 1
-        }
-    }
-    
-    private func addPianoKeysWithCurrentFrames() {
-        for key in pianoKeys {
-            key.removeFromSuperview()
-        }
-        
-        var whiteKeyNum = 0
-        for key in pianoKeys {
-            let keyFrame: CGRect!
-            if key.keyType == PianoKey.KeyType.White {
-                keyFrame = CGRect(x: CGFloat(whiteKeyNum) * whiteKeyWidth, y: 0, width: whiteKeyWidth, height: whiteKeyHeight)
-                whiteKeyNum += 1
-            } else {
-                let offset = blackKeyOffset[(whiteKeyNum - 1) % 7]
-                keyFrame = CGRect(x: CGFloat(whiteKeyNum - 1) * whiteKeyWidth + whiteKeyWidth * (offset/7.0), y: 0, width: blackKeyWidth, height: blackKeyHeight)
-            }
-            key.frame = keyFrame
-            addSubview(key)
-        }
-        
-        // put black keys on top
-        for key in pianoKeys {
-            if key.keyType == PianoKey.KeyType.Black {
-                bringSubviewToFront(key)
-            }
-        }
-    }
-    
     
     func playNoteFromKeyboard(sender: PianoKey) -> Void {
         
